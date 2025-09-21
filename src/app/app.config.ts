99e9847c39f1852acabc06f841b3fa8bf3eb9c09
marwanfathi250@gmail.com <marwanfathi250@gmail.com>
2025-09-21 17:21:44 +0300

    fix: use HashLocationStrategy for Angular routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,15 +6,18 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
     provideAnimations(),
     provideToastr(),
     provideHttpClient(withFetch()),
-    importProvidersFrom([CookieService ])
+    importProvidersFrom([CookieService]),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }  // 👈 هنا الإضافة
   ]
-
 };
